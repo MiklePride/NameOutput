b4db08976c109d1ffaf92eb7d02e8dad82da5675
Михаил Денисов <migelius.invesy@gmail.com>
2022-03-23 09:04:03 +0300

    Добавьте файлы проекта.

---
+++ NameOutput/Program.cs
@@ -0,0 +1,26 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        string userName;
+        char userSymbol;
+
+        Console.WriteLine("Введите имя:");
+        userName = Console.ReadLine();
+        Console.WriteLine("Введите символ:");
+        userSymbol = Convert.ToChar(Console.ReadLine());
+
+        int numberSymbolsUpperLineFrame = userName.Length;
+        int numberSymbolsLowerLineFrame = userName.Length;
+
+        for(int i = 0; i < numberSymbolsUpperLineFrame; i++)
+        {
+            Console.Write(userSymbol);
+        }
+        Console.WriteLine($"\n{userSymbol}{userName}{userSymbol}");
+        for(int i = 0; i < numberSymbolsLowerLineFrame; i++)
+        {
+            Console.Write(userSymbol);
+        }
+    }
+}
